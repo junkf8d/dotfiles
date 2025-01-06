52007ef92e78b75cda7249d7a3ed9b138e158d61
junkf8d <junkf8d@gmail.com>
2025-01-07 00:17:05 +0900

    gitのppaが追加済みなら追加しないようにした

--- setup/git.sh
+++ setup/git.sh
@@ -1,8 +1,10 @@
 source $HOME/dotfiles/_common.sh
 
-sudo add-apt-repository ppa:git-core/ppa -y
-sudo apt update
-sudo apt install git -y
+if ! grep -qr "git-core/ppa" /etc/apt/sources.list /etc/apt/sources.list.d/ 2>/dev/null; then
+  sudo add-apt-repository ppa:git-core/ppa -y
+  sudo apt update
+  sudo apt install git -y
+fi
 
 if ! git config --get --global user.name; then
   git config --global user.name $(meta .git.user.name)
