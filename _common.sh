c20ba1e911794527df3d5280b849c30b244e57cb
junkf8d <93854615+junkf8d@users.noreply.github.com>
2025-01-07 01:33:42 +0900

    フラグを管理する関数を追加

--- _common.sh
+++ _common.sh
@@ -5,3 +5,19 @@ meta() {
 log() {
     echo "$(meta '.meta.prompt')$1"
 }
+
+get_flag(){
+    jq -r $1 $HOME/dotfiles/.flags.json
+}
+
+set_flag(){
+    # これでも出来るけどエラー出ると全部消えるからやらない
+    #cat <<< $(jq "$1 |= \"$2\"" $HOME/dotfiles/.flags.json) > $HOME/dotfiles/.flags.json
+
+    result=$(jq "$1 |= \"$2\"" $HOME/dotfiles/.flags.json)
+    if [ $? -eq 0 ]; then
+        echo $result > $HOME/dotfiles/.flags.json
+    else
+        echo " * json の書き換えに失敗しました: $1 = $2"
+    fi
+}
