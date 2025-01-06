4a6ae2873277096ffd44d7897c6ce526eee6bafc
junkf8d <93854615+junkf8d@users.noreply.github.com>
2025-01-07 03:24:30 +0900

    一括で実行するスクリプトを追加

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# sudoでなきゃ終わり
+if [ "$EUID" -ne 0 ]; then
+  echo " * Please run as root."
+  exit 1
+fi
+
+# WSL Ubunutでなきゃ終わり
+if ! (grep -qi microsoft /proc/version && grep -qi ubuntu /etc/os-release); then
+  echo " * This script is only for WSL Ubuntu."
+  exit 1
+fi
+
+
+# 必須パッケージをインストール
+sudo apt update
+sudo apt upgrade -y
+sudo apt install jq wl-clipboard
+
+echo " * Starting setup for WSL Ubuntu..."
+
+ROOT=$(git rev-parse --show-toplevel)
+source $ROOT/_common.sh
+
+chmod +x ./setup/*.sh
+for script in $ROOT/setup/*.sh; do
+    if [ -f "$script" ]; then
+        echo "---------- $(basename "$script")"
+        "$script"
+    else
+        echo "スクリプトが見つかりません。"
+    fi
+done
